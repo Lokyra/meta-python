8854474625076d92daf362b7742fcc9d2852f89e
ayoub <ayoubousbia02@gmail.com>
2024-06-12 16:08:23 +0200

    for/while loop in python

---
+++ conditionals/python_loops.py
@@ -0,0 +1,12 @@
+favorites = ['Creme Brulee', 'Apple Pie', 'Churros', 'Tiramisu', 'Chocolate Cake'] 
+
+
+for idx, item in enumerate(favorites):
+    print(idx, item)
+
+
+count = 0
+
+while count < len(favorites):
+    print('I like this desert', favorites[count])
+    count += 1
